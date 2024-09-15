2a18b1e5a8b799029d983332efac8d1c2ff47ab4
TomWu <tom.wu@te.com>
2024-09-15 17:35:01 +0800

    change2

--- MFCApplication1/MFCApplication1/MFCApplication1View.cpp
+++ MFCApplication1/MFCApplication1/MFCApplication1View.cpp
@@ -34,11 +34,10 @@ END_MESSAGE_MAP()
 // CMFCApplication1View 构造/析构
 
 CMFCApplication1View::CMFCApplication1View() noexcept
-{	//master
-	//master
+{
 	// TODO: 在此处添加构造代码
-	//master
-	//master
+	//br1:
+	//br1;
 }
 
 CMFCApplication1View::~CMFCApplication1View()
